580d2411c59c4df413322cbd5f8a051038752a47
v.kalmykov <v.kalmykov@tinkoff.ru>
2022-10-18 19:56:34 +0300

    update --help

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
-use serde::Deserialize;
 use clap::Parser;
+use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct Env {
@@ -9,6 +9,7 @@ pub struct Env {
 #[derive(Parser)]
 pub struct Args {
     #[clap(value_parser)]
-    /// Example: http://localhost:3000
+    /// Example: http://localhost:3000.
+    /// Org is detrmined by api token, so http://localhost:3000?orgId=123 is invalid
     pub grafana_url: String,
 }
